ee157be92c49d8184626ab0f6a3bd4ff4d171ec7
Roshan76143 <cholasuvai@gmail.com>
2025-11-24 22:17:32 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:"Arial", padding:"20px"}}>
+      <h1>Welcome to Chola Suvai</h1>
+      <p>Your authentic traditional foods marketplace.</p>
+    </div>
+  );
+}
