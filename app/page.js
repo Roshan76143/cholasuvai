1101d19b54a94c03228ba51dcad34a0098241d1a
Roshan76143 <cholasuvai@gmail.com>
2025-11-24 22:49:22 +0530

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{fontFamily:"Arial", padding:"20px"}}>
-      <h1>Welcome to Chola Suvai</h1>
-      <p>Your authentic traditional foods marketplace.</p>
-    </div>
-  );
-}
