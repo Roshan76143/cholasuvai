872d33e6f90432c2bf99aa4ca3902d591769a48e
Roshan76143 <cholasuvai@gmail.com>
2025-11-24 22:22:07 +0530

    Create page.js

---
+++ app/app/app/admin/page.js
@@ -0,0 +1,8 @@
+export default function AdminLogin() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial' }}>
+      <h1>Admin Login</h1>
+      <p>This page will contain secure admin login form.</p>
+    </div>
+  );
+}
