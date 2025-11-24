56e47442e660057258b7644156900518c65e7278
Roshan76143 <cholasuvai@gmail.com>
2025-11-24 22:22:51 +0530

    Create page.js

---
+++ app/app/app/admin/app/staff/page.js
@@ -0,0 +1,8 @@
+export default function StaffLogin() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial' }}>
+      <h1>Staff Login</h1>
+      <p>This page will contain staff login form.</p>
+    </div>
+  );
+}
